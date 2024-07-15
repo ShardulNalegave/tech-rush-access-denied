2ec8dae922b581a3af5ed65c1898a114b5f0c708
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-07-15 23:30:25 +0530

    Setup prisma, redis and meilisearch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { createClient } from 'redis';
+import MeiliSearch from 'meilisearch';
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+const prisma = new PrismaClient();
+const redis = createClient();
+const meilisearch = new MeiliSearch({
+  host: 'http://127.0.0.1:7700',
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
